2815c89f4f4eaabe8962545e8b91a6ed4fd0b87c
Jakub Mrowczynski <jakub.mrowczynski@allegro.com>
2024-12-19 13:23:02 +0100

    changes for task 2 with sorting and more examples

--- task2.py
+++ task2.py
@@ -24,7 +24,7 @@ class ToDoList:
         return False
 
     def view_sorted_tasks(self):
-        return sorted(self.tasks, key=lambda x: x.title)
+        return sorted(self.tasks, key=lambda x: x.priority, reverse=True)
 
     def mark_as_completed(self, title):
         for task in self.tasks:
@@ -41,6 +41,9 @@ if __name__ == "__main__":
     todo_list.add_task(Task("Task 3", "Description 3", 3, False))
     todo_list.add_task(Task("Task 4", "Description 4", 4, False))
     todo_list.add_task(Task("Task 5", "Description 5", 5, False))
+
+    todo_list.add_task(Task("new task for testing", "New description", 100, False))
+
     print(todo_list.view_sorted_tasks())
     todo_list.mark_as_completed("Task 3")
     print(todo_list.view_sorted_tasks())
